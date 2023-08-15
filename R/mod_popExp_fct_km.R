fb93a29eb7600591ab2f95640bca9fca1f270b79
joeeerere <108251919+joeeerere@users.noreply.github.com>
2023-08-15 18:22:15 +0900

    km plot 수정

--- R/mod_popExp_fct_km.R
+++ R/mod_popExp_fct_km.R
@@ -11,78 +11,82 @@
 #' @param group character, variable name of grouping variable (categorical or factor)
 #' @param points logical, whether to plot + symbols when patients censored
 #' @param ci logical, whether the curve(s) should be accompanied with a 95\% CI
-#'
+#' @param table logical, whether the table should be shown
+#' @param pval logical, whether the p-value should be shown
+#' @param timeval character, Choice option to display duration(time) as Day , Month , Year
+#' @param timeby Numeric, Set interval of duration(time)
+
 #' @importFrom stats as.formula
 #' @importFrom GGally ggsurv
 #' @importFrom survival survfit Surv
-#'
+#' @import jskm
 #' @family popExp functions
 #' @keywords popEx
 #'
 #' @return A ggplot object containing the KM curve plot
 #'
 #' @noRd
-app_km_curve <- function(data, yvar, resp_var, cnsr_var, group = "NONE", points = TRUE, ci = FALSE) {
+app_km_curve <- function(data, yvar, resp_var, cnsr_var, group = "NONE", points = TRUE, ci = FALSE, table=FALSE, pval=FALSE, timeval, timeby) {
   resp_var_sym <- rlang::sym(resp_var)
-
+  
   # Filter data by param selected
   suppressWarnings(
     d <- data %>%
       dplyr::filter(PARAMCD == yvar) %>%
       dplyr::select(USUBJID, PARAM, PARAMCD, one_of(resp_var), one_of(cnsr_var), one_of(group)) %>%
       filter(!is.na(!!resp_var_sym)) %>%
+      dplyr::mutate(
+        cnsr_var = case_when(
+          cnsr_var == 0 ~ 1,
+          cnsr_var == 1 ~ 0
+        )
+      ) %>%
       dplyr::distinct()
   )
-  # print(d)
-  # if(group != "NONE"){
-  #   group_counts <-
-  #     d %>%
-  #     group_by_at(vars(one_of(group))) %>%
-  #     summarize(n = n())
-  #   print(group_counts)
-  # }
+  
 
-  fit_formula <- paste0(
+  if(timeval=="Month"){
+    d[[resp_var]] <- d[[resp_var]]/30.4
+  }else if(timeval=="Year"){
+    d[[resp_var]] <- d[[resp_var]]/365
+  }
+
+  fit_formula <- as.formula(paste0(
     "survival::Surv(", resp_var, ", ", cnsr_var, ") ~ ",
     if (group != "NONE" & !rlang::is_empty(group)) group else 1
-  )
-  fit <- survival::survfit(as.formula(fit_formula), data = d)
-  # print(fit_formula)
-  # print(fit)
-  # print(summary(fit))
-
+  ))
+  
+  fit <- survival::survfit(fit_formula, data = d)
+  fit$call$formula <- fit_formula
+  
+  
   # Initialize title of variables plotted
   # if group used, include those "by" variables in title
-  by_title <- case_when(
+  by_title <- dplyr::case_when(
     group != "NONE" ~ paste("\nby", best_lab(data, group)),
     TRUE ~ ""
   )
-
+  
   # generate plot
   p <-
-    GGally::ggsurv(fit,
-      order.legend = FALSE, # use data order, not survival order
-      CI = ci,
-      plot.cens = points
-    ) +
-    # survminer::ggsurvplot(fit,conf.int = ci) + # didn't work
-    ggplot2::xlab(glue::glue("{unique(d$PARAM)}")) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      text = ggplot2::element_text(size = 12),
-      axis.text = ggplot2::element_text(size = 12),
-      plot.title = ggplot2::element_text(size = 16)
-    ) +
-    ggplot2::ggtitle(
-      paste(unique(d$PARAM), by_title)
-      # ,subtitle = paste(by_title) # plotly won't automatically accept this
-    )
-
+    jskm::jskm(fit, marks = points, ci = ci,
+               table = table, surv.scale='percent', data = d,
+               cumhaz = T, pval =pval, pval.coord = c(15,1.15),
+               pval.size = 5, timeby = timeby, ylims = c(0,1.2),
+               main= paste(unique(d$PARAM), by_title),
+               legendposition=c(0.15,0.8),  xlab = paste(unique(d$PARAM), paste0("(",timeval,")")))
+  
+  p <- p + ggplot2::theme(
+    text = ggplot2::element_text(size = 12),
+    axis.text = ggplot2::element_text(size = 12),
+    plot.title = ggplot2::element_text(size = 16)
+  )
+  
   # Add in plot layers conditional upon user selection
   if (by_title != "") {
     p <- p + ggplot2::theme(plot.margin = ggplot2::margin(t = 1.2, unit = "cm")) #+
     # survminer::ggsurvplot_group_by(fit, group.by = group)
   }
-
+  
   return(p)
 }
--- R/mod_popExp_km.R
+++ R/mod_popExp_km.R
@@ -25,14 +25,22 @@ km_ui <- function(id, label = "km") {
         column(6, selectInput(ns("group"), "Group By", choices = "NONE", selected = "NONE")),
         column(6, selectInput(ns("cnsr_var"), "Censor Variable (0,1)", choices = "CNSR", selected = "CNSR"))
       ),
+      shinyWidgets::radioGroupButtons(ns("timeval"),  "Display duration as", c("Day", "Month", "Year"), selected = "Day"
+      ),
+      numericInput(ns("timeby"), "Time by", value=30),
       shinyWidgets::materialSwitch(ns("points"), h6("Mark censored observations?"),
         status = "primary", value = TRUE
       ),
       shinyWidgets::materialSwitch(ns("ci"), h6("Include 95% confidence interval?"),
         status = "primary", value = FALSE
+      ),
+      shinyWidgets::materialSwitch(ns("table"), h6("Show table?"),
+                                   status = "primary", value = FALSE
+      ),
+      shinyWidgets::materialSwitch(ns("pval"), h6("Show P-value?"),
+                                   status = "primary", value = FALSE
       )
-      # checkboxInput(ns("points"), "Mark censored observations?", value = TRUE),
-      # checkboxInput(ns("ci"), "Include 95% confidence interval?", value = FALSE)
+      
     )
   )
 }
@@ -97,11 +105,7 @@ km_srv <- function(input, output, session, data, run) {
   # certain param
   observeEvent(input$yvar, {
     req(run(), input$yvar != "")
-
-    # col <- c(1:4)
-    # col <- c(0, 1, 1, 1)
-    # col <- c(1, 1, 1)
-
+    
     d0 <- data()
     my_cvars <- d0 %>%
       dplyr::filter(PARAMCD == input$yvar) %>%
@@ -120,35 +124,46 @@ km_srv <- function(input, output, session, data, run) {
   })
 
 
-  observeEvent(input$yvar, {
-    req(run(), input$yvar != "")
+  observeEvent(input$timeval, {
+    req(run(), input$timeval != "")
 
-    # yvar paramcd
-    group_dat <- data() %>%
-      dplyr::filter(PARAMCD == input$yvar) %>%
-      select_if(~ !all(is.na(.))) # remove NA cols
+    if(input$timeval == "Day"){
+      
+      updateNumericInput(session, "timeby",
+                        min = 1,
+                         value = 30)
+      }else if (input$timeval == "Month"){
+      updateNumericInput(session, "timeby",
+                         min = 1,
+                         value = 1)
+        }else if (input$timeval == "Year"){
+          updateNumericInput(session, "timeby",
+                             min = 1,
+                             value = 1)
+        }
 
-    # character and factor columns for grouping or faceting (separating)
-    char_col <- subset_colclasses(group_dat, is.character)
-    fac_col <- subset_colclasses(group_dat, is.factor)
-    group <- sort(c(fac_col, char_col))
-    # print("char_col:")
-    # print(char_col)
-    # print("fac_col:")
-    # print(fac_col)
-    # print(".")
-    # print(".")
-
-    # remove some variables...
-    grp <- group[!(group %in% c("data_from", "PARAM", "PARAMCD", "USUBJID"))]
-
-    # populate dropdowns with choices
-    updateSelectInput(session, "group",
-      choices = c("NONE", grp),
-      selected = isolate(input$group)
-    )
   })
 
+  observeEvent(input$yvar, {
+    req(run(), input$yvar != "")
+    
+    d <- data()
+    my_vars <- d %>%
+      dplyr::filter(PARAMCD == input$yvar) %>%
+      dplyr::filter(data_from == "ADTTE") %>% # Numeric visit var has to exist in TTE data
+      select(one_of("AVISITN", "VISITNUM"), ends_with("DY")) %>%
+      select_if(~ !all(is.na(.))) %>% # remove NA cols
+      colnames()
+    
+    updateSelectInput(
+      session = session,
+      inputId = "resp_var",
+      choices = c("AVAL", my_vars),
+      selected = isolate(input$resp_var)
+    )
+  })
+  
+  
   # create plot object using the numeric column on the yaxis
   # or by filtering the data by PARAMCD, then using AVAL or CHG for the yaxis
   p <- reactive({
@@ -161,9 +176,12 @@ km_srv <- function(input, output, session, data, run) {
       input$cnsr_var,
       input$group,
       input$points,
-      input$ci
+      input$ci,
+      input$table,
+      input$pval,
+      input$timeval,
+      input$timeby
     )
   })
-
   return(p)
 }
